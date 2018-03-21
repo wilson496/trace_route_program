c2e5d762c1ba0baec76949923e1da2dc0ac33226
Unknown <cw4886130@gmail.com>
2018-03-21 15:11:14 -0700

    Stuffs
    things

--- traceroute.py
+++ traceroute.py
@@ -1,36 +1,88 @@
 import sys
 import dpkt
-
+import socket
 
 """
 Author: Cameron Wilson
 Student #: V00822184
-Date submitted: March 5th, 2018
+Date submitted: March 28th, 2018
 """
 
 
-'''
-SOURCE:
-https://github.com/jeffsilverm/dpkt_doc/blob/master/decode_tcp.py#L10
+class IPSession:
 
-This source was used to help with using tuples as keys to manage state information for the different connections 
-'''
+    #List of ip packets
+    ip_list = []
+    ORIGIN = ''
+    ULT_DEST = ''
 
+    def __init__(self):
+        pass
+
+    def append(self, packet):
+        self.ip_list.append(packet)
+
+    def get_list(self):
+        return self.ip_list
+
+    def set_ult_dest(self, ult_dest):
+        self.ULT_DEST = ult_dest
+
+    def set_origin(self, origin):
+        self.ORIGIN = origin
+
+    def get_ult_dest(self):
+        return self.ULT_DEST
+
+    def get_origin(self):
+        return self.ORIGIN
 
 def main(f):
 
     """This function decodes a packet capture file f and breaks it up into tcp connections"""
     pcap = dpkt.pcapng.Reader(f)
-    packet_counter = 0
 
+
+    ip_sess = IPSession()
+
+
+    #Separate IP packets from traceroute file
     for ts, buf in pcap:
-        print(ts, buf)
+
+        eth = dpkt.ethernet.Ethernet(buf)
+        ip = eth.data
+        #udp = ip.data
+
+        if isinstance(ip, dpkt.ip.IP):
+            if isinstance(ip.data, dpkt.udp.UDP) or isinstance(ip.data, dpkt.icmp.ICMP):
+                ip_sess.append((ts, ip))
+
+    #Iterate through list in IPSession and do stuffs
+    ip_list = ip_sess.get_list()
+
+    #ip_header = ip.__hdr__
+    #print(ip.__hdr__)
 
 
+    #Set the origin and ultimate (final) destination for the traceroute
+    ip_sess.set_origin(ip_list[0][1].src)
+    ip_sess.set_ult_dest(ip_list[0][1].dst)
+
+    for packet in ip_list:
+        ip = packet[1]
+        udp = packet[1].data
 
 
+        connection_id = (ip.src, ip.dst)
+        # print(connection_id)
+        #print(socket.inet_ntoa(connection_id[0]))
+        #print(socket.inet_ntoa(connection_id[1]))
 
+    print(socket.inet_ntoa(ip_sess.get_origin()))
+    print(socket.inet_ntoa(ip_sess.get_ult_dest()))
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     f = open(sys.argv[1], 'rb')
     main(f)
+
+
