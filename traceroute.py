fb8f8f603043562d0c2d33e7c76cdd1ac9c6cbf8
Unknown <cw4886130@gmail.com>
2018-03-23 10:52:36 -0700

    Added functions to ip_session class

--- traceroute.py
+++ traceroute.py
@@ -15,12 +15,17 @@ class IPSession:
     ip_list = []
     ORIGIN = ''
     ULT_DEST = ''
+    offset = 0
+    fragment_count = 0
 
     def __init__(self):
         pass
 
     def append(self, packet):
         self.ip_list.append(packet)
+
+    def set_list(self, new_list):
+        self.ip_list = new_list
 
     def get_list(self):
         return self.ip_list
@@ -37,49 +42,128 @@ class IPSession:
     def get_origin(self):
         return self.ORIGIN
 
+    def set_offset(self, new_offset):
+        self.offset = new_offset
+
+    def get_offset(self):
+        return self.offset
+
+    def get_fragment_count(self):
+        return self.fragment_count
+
+    def inc_fragment_count(self):
+        self.fragment_count += 1
+
+
 def main(f):
 
     """This function decodes a packet capture file f and breaks it up into tcp connections"""
     pcap = dpkt.pcapng.Reader(f)
-
-
     ip_sess = IPSession()
 
+    udp_tracker = {}
+    icmp_tracker = {}
+
+    i = 0
+
+    protocol_tracker = {}
 
     #Separate IP packets from traceroute file
-    for ts, buf in pcap:
 
+    for ts, buf in pcap:
+        i +=1
         eth = dpkt.ethernet.Ethernet(buf)
         ip = eth.data
-        #udp = ip.data
+
 
         if isinstance(ip, dpkt.ip.IP):
             if isinstance(ip.data, dpkt.udp.UDP) or isinstance(ip.data, dpkt.icmp.ICMP):
                 ip_sess.append((ts, ip))
-
-    #Iterate through list in IPSession and do stuffs
-    ip_list = ip_sess.get_list()
-
-    #ip_header = ip.__hdr__
-    #print(ip.__hdr__)
+                protocol_tracker[ip.p] = ip.get_proto(ip.p).__name__
 
 
     #Set the origin and ultimate (final) destination for the traceroute
-    ip_sess.set_origin(ip_list[0][1].src)
-    ip_sess.set_ult_dest(ip_list[0][1].dst)
+    ip_sess.set_origin(ip_sess.get_list()[0][1].src)
+    ip_sess.set_ult_dest(ip_sess.get_list()[0][1].dst)
 
-    for packet in ip_list:
+    #TODO: Fragmentation. Look for if the More Fragments (MF) flag is set to determine if packets have been fragmented.
+    # If none of them have the MF flag, then keep the offset at 0
+
+    #If this fires, we are dealing with Windows, so filter out the UDP
+    if isinstance(ip_sess.get_list()[0][1].data, dpkt.icmp.ICMP) and ip_sess.get_list()[0][1].ttl == 1:
+        ip_sess.set_list([packet for packet in ip_sess.get_list() if isinstance(packet[1].data, dpkt.icmp.ICMP)])
+        windows(ip_sess)
+
+    else:
+        linux(ip_sess)
+
+
+    # Print results of processing the traceroute file
+    # print_out(ip_sess, protocol_tracker)
+
+def windows(ip_sess):
+    pass
+
+
+def linux(ip_sess):
+    for packet in ip_sess.get_list():
         ip = packet[1]
-        udp = packet[1].data
+        data = packet[1].data
+
+        # print(dir(ip))
+        # print(ip.mf)
+        # sys.exit()
 
 
-        connection_id = (ip.src, ip.dst)
+
+        # print(ip.off)
+        # TODO: Account for the ICMP not containing the ports of either source or destination. However, UDP packets do have source and destination ports
+
+        # if isinstance(ip.data, dpkt.udp.UDP):
+        #     connection_id = (ip.src, udp.sport, ip.dst, udp.dport)
+        #
+        # elif isinstance(ip.data, dpkt.icmp.ICMP):
+        #     connection_id = (ip.src, ip.dst)
+
         # print(connection_id)
-        #print(socket.inet_ntoa(connection_id[0]))
-        #print(socket.inet_ntoa(connection_id[1]))
 
-    print(socket.inet_ntoa(ip_sess.get_origin()))
-    print(socket.inet_ntoa(ip_sess.get_ult_dest()))
+        # print("SOURCE: " + str(socket.inet_ntoa(connection_id[0])))
+        # print("DEST: " + str(socket.inet_ntoa(connection_id[1])))
+
+
+
+
+def print_out(ip_sess, protocol_tracker):
+
+    print("The IP address of the source node: " + str(socket.inet_ntoa(ip_sess.get_origin())))
+    print("The IP address of the ultimate destination node: " + str(socket.inet_ntoa(ip_sess.get_ult_dest())))
+
+    # print('The IP addresses of the intermediate destination nodes: ')
+    '''For each of the intermediate ips, print them out'''
+
+
+    print('The values in the protocol field of IP headers:')
+
+    key_list = sorted(list(protocol_tracker.keys()))
+    for key in key_list:
+        print("\t{0}: {1}".format(key, protocol_tracker[key]))
+
+
+
+    print('The number of fragments created from the original datagram is: ' + str(ip_sess.get_fragment_count()))
+
+    print('The offset of the last fragment is: ' + str(ip_sess.get_offset()))
+    
+
+
+
+    '''
+    For each pairing (i.e. origin to intermediate, to final)
+    find the average Round trip time 
+    '''
+
+
+
 
 if __name__ == "__main__":
     f = open(sys.argv[1], 'rb')
