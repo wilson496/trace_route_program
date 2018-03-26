8a8f231aa25fa9169a2790562dc68560efb24df9
Unknown <cw4886130@gmail.com>
2018-03-25 17:14:07 -0700

    Removed pcap_reader
    added support for windows and linux

--- traceroute.py
+++ traceroute.py
@@ -58,28 +58,45 @@ class IPSession:
 def main(f):
 
     """This function decodes a packet capture file f and breaks it up into tcp connections"""
-    pcap = dpkt.pcapng.Reader(f)
+
+
+    #TODO: Be able to read the fragmented header. Its apparently pcap format, but the tcpdump header is invalid
+    try:
+        pcap = dpkt.pcapng.Reader(f)
+    except ValueError:
+        pcap = dpkt.pcap.Reader(f)
+    except Exception:
+        print('Error opening pcap/pcapng file! Please ensure the file used follows either of these formats.')
+        sys.exit()
+
     ip_sess = IPSession()
 
     udp_tracker = {}
     icmp_tracker = {}
+
 
     i = 0
 
     protocol_tracker = {}
 
     #Separate IP packets from traceroute file
-
     for ts, buf in pcap:
-        i +=1
+
         eth = dpkt.ethernet.Ethernet(buf)
         ip = eth.data
+        data = ip.data
 
-
+        #TODO: Filter out SSDP, DP-LSP-DISC, ICMPv6
         if isinstance(ip, dpkt.ip.IP):
-            if isinstance(ip.data, dpkt.udp.UDP) or isinstance(ip.data, dpkt.icmp.ICMP):
-                ip_sess.append((ts, ip))
-                protocol_tracker[ip.p] = ip.get_proto(ip.p).__name__
+
+            try:
+                dns = dpkt.dns.DNS(data.data)
+                i += 1
+                continue
+            except:
+                if isinstance(ip.data, dpkt.udp.UDP) or isinstance(ip.data, dpkt.icmp.ICMP):
+                    protocol_tracker[ip.p] = ip.get_proto(ip.p).__name__
+                    ip_sess.append((ts, ip))
 
 
     #Set the origin and ultimate (final) destination for the traceroute
@@ -87,61 +104,159 @@ def main(f):
     ip_sess.set_ult_dest(ip_sess.get_list()[0][1].dst)
 
     #TODO: Fragmentation. Look for if the More Fragments (MF) flag is set to determine if packets have been fragmented.
-    # If none of them have the MF flag, then keep the offset at 0
+    #TODO: If none of them have the MF flag, then keep the offset at 0
+
 
     #If this fires, we are dealing with Windows, so filter out the UDP
     if isinstance(ip_sess.get_list()[0][1].data, dpkt.icmp.ICMP) and ip_sess.get_list()[0][1].ttl == 1:
         ip_sess.set_list([packet for packet in ip_sess.get_list() if isinstance(packet[1].data, dpkt.icmp.ICMP)])
-        windows(ip_sess)
+        hop_tracker = windows(ip_sess)
 
     else:
-        linux(ip_sess)
-
+        hop_tracker = linux(ip_sess)
 
     # Print results of processing the traceroute file
-    # print_out(ip_sess, protocol_tracker)
+    print_out(ip_sess, protocol_tracker, hop_tracker)
 
 def windows(ip_sess):
-    pass
+
+    connection_list = []
+    hop_tracker = {}
 
 
-def linux(ip_sess):
     for packet in ip_sess.get_list():
         ip = packet[1]
         data = packet[1].data
 
-        # print(dir(ip))
-        # print(ip.mf)
+
+        #Check if there are fragments, denoted by the MF (More Fragments) bit being set
+        if ip.mf == 1:
+            ip_sess.inc_fragment_count()
+
+        elif ip_sess.get_fragment_count() > 0:
+            #Last fragment
+            if ip.mf == 0:
+                ip_sess.set_offset(ip.off)
+                ip_sess.inc_fragment_count()
+
+        # print(dir(data.data.seq))
+        # print(data.type)
         # sys.exit()
 
+        #Echo request
+        if data.type == 8:
+            connection_id = (ip.src, ip.dst, data.data.seq, ip.ttl)
+            connection_list.append(connection_id)
+
+        #Echo reply
+        elif data.type == 0:
+            pass
+
+        #TTL exceeded
+        elif data.type == 11:
+
+            for each in list(set(connection_list)):
+                if data.timeexceed.data.icmp.data.seq == each[2]:
+                    hop_tracker[ip.src] = each[2]
+
+    hop_tuples = []
+    for each in hop_tracker:
+        hop_tuples.append((hop_tracker[each], socket.inet_ntoa(each)))
+
+    return sorted(hop_tuples, key=lambda x: x[0])
 
 
-        # print(ip.off)
+
+def linux(ip_sess):
+
+    connection_list = []
+    hop_tracker = {}
+    q = ip_sess.get_list()
+
+    for packet in q:
+
+        ip = packet[1]
+        data = packet[1].data
+
+
+        #Check if there are fragments, denoted by the MF (More Fragments) bit being set
+        if ip.mf == 1:
+            ip_sess.inc_fragment_count()
+
+        elif ip_sess.get_fragment_count() > 0:
+            #Last fragment
+            if ip.mf == 0:
+                ip_sess.set_offset(ip.off)
+                ip_sess.inc_fragment_count()
+
+
         # TODO: Account for the ICMP not containing the ports of either source or destination. However, UDP packets do have source and destination ports
 
-        # if isinstance(ip.data, dpkt.udp.UDP):
-        #     connection_id = (ip.src, udp.sport, ip.dst, udp.dport)
-        #
-        # elif isinstance(ip.data, dpkt.icmp.ICMP):
-        #     connection_id = (ip.src, ip.dst)
+        if isinstance(ip.data, dpkt.udp.UDP):
+            connection_id = (ip.src, data.sport, ip.dst, data.dport, ip.ttl)
+            connection_list.append(connection_id)
 
-        # print(connection_id)
+        elif isinstance(ip.data, dpkt.icmp.ICMP):
 
-        # print("SOURCE: " + str(socket.inet_ntoa(connection_id[0])))
-        # print("DEST: " + str(socket.inet_ntoa(connection_id[1])))
+            #If this field is present, then ICMP is a time exceeded packet for TTL. Otherwise, destination is unreachable
+            if data.type == 11 and data.code == 0:
 
-
+                source_match = ip.data.timeexceed.data.udp.sport
 
 
-def print_out(ip_sess, protocol_tracker):
+                #IDEA: There is a possibility that certain routers added to the list are NOT in the path to the ult dest
+
+
+                #This MAY not work. TTL may be out of order for ips
+                for each in list(set(connection_list)):
+                    if source_match == each[1]:
+                        hop_tracker[ip.src] = each[4]
+
+
+            #If the destination is unreachable, you have a ttl long enough to get to the ultimate_destination
+            # BUT, the ultimate_destination is not listening on that port, thus giving the error.
+
+            elif data.type == 3:
+                # print("Destination unreachable")
+                pass
+
+
+    '''
+    
+        
+        WRONG - router 12: 74.125.37.91
+        router 13: 72.14.237.123
+        WRONG - router 14: 209.85.250.121
+        WRONG - router 15: 209.85.249.155
+        router 16: 209.85.249.153
+
+
+    '''
+
+
+        # print("SOURCE: " + str(socket.inet_ntoa(connection_id[0])) + " | DEST: " + str(socket.inet_ntoa(connection_id[1])) + " | TTL: " + str(ip.ttl))
+        # print('\t | MF:' + str(ip.mf) + " | TYPE: " + str(ip.get_proto(ip.p).__name__))
+
+
+    hop_tuples = []
+    for each in hop_tracker:
+        hop_tuples.append((hop_tracker[each], socket.inet_ntoa(each)))
+
+    return sorted(hop_tuples, key=lambda x: x[0])
+
+
+def print_out(ip_sess, protocol_tracker, hop_tracker):
 
     print("The IP address of the source node: " + str(socket.inet_ntoa(ip_sess.get_origin())))
     print("The IP address of the ultimate destination node: " + str(socket.inet_ntoa(ip_sess.get_ult_dest())))
 
-    # print('The IP addresses of the intermediate destination nodes: ')
-    '''For each of the intermediate ips, print them out'''
+    print('The IP addresses of the intermediate destination nodes: ')
+    i = 0
+    for each in hop_tracker:
+        i += 1
+        print('\trouter {0}: '.format(i) + str(each[1]) + " TTL: " + str(each[0]))
 
-
+    print("")
     print('The values in the protocol field of IP headers:')
 
     key_list = sorted(list(protocol_tracker.keys()))
@@ -149,20 +264,16 @@ def print_out(ip_sess, protocol_tracker):
         print("\t{0}: {1}".format(key, protocol_tracker[key]))
 
 
-
+    print("")
     print('The number of fragments created from the original datagram is: ' + str(ip_sess.get_fragment_count()))
 
     print('The offset of the last fragment is: ' + str(ip_sess.get_offset()))
     
 
-
-
     '''
     For each pairing (i.e. origin to intermediate, to final)
     find the average Round trip time 
     '''
-
-
 
 
 if __name__ == "__main__":
